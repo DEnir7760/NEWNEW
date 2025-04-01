9985cfd346d6170400429141d77a8399f74707b1
DEnir7760 <155641612+DEnir7760@users.noreply.github.com>
2025-04-01 21:31:01 +0200

    stworzenie całkowicie strony

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,76 @@
-
 import './App.css'
 
 function App() {
-  
+
+const n=1;
+console.log(n);
+
+const z = 10;
+
+const x = 2;
+const y = 3;
+
+let v1 = n3+2z-n;
+
+let w1 = n1;
+let w2 = n2;
+let w3 = n3;
+let w4 = n4;
+let w5 = n5;
+let w6 = n6;
+let w7 = n7;
+let w8 = n8;
+let w9 = n9;
+let w10 = n10;
+
+let r1 = 15x**3+1-15y;
+let r2 = 2x+315-4x2;
+let r3 = 3*y2-15x**2y-7y;
+let r4 = 2xy+15xy;
+let r5 = 3x*2+15y+2;
 
   return (
-    <>
-     Hello , World;
-    </>
+      <>
+          <header>
+              <div class="podpis">Mój komputer ma numer {n}</div>
+
+</header>
+    <div class="par">
+            <div class="rown ro2">
+                <p>
+                {n} * 1 = {w1}<br></br>
+                {n} * 2 = {w2}<br></br>
+                {n} * 3 = {w3}<br></br>
+                {n} * 4 = {w4}<br></br>
+                {n} * 5 = {w5}<br></br>
+                {n} * 6 = {w6}<br></br>
+                {n} * 7 = {w7}<br></br>
+                {n} * 8 = {w8}<br></br>
+                {n} * 9 = {w9}<br></br>
+                {n} * 10 = {w10}<br></br>
+                </p>
+          </div>
+
+              <div class="rown ro1">
+                  <p>z = {z}</p><br></br>
+                <p>{n} * 3 + 2z - {n} = {v1}</p>
+          </div>
+
+
+              <div class="rown ro3">
+                  <p>x = {x} y = {y}</p><br></br>
+
+                  <p>
+                    15x<sup>3</sup>+1-15y = {r1}<br></br>
+                    2x+315-4x<sup>2</sup> = {r2}<br></br>
+                    3y<sup>2</sup>-15x<sup>2</sup>y-7y = {r3}<br></br>
+                    2xy+15xy; = {r4}<br></br>
+                    3x<sup>2</sup>+15y+2 = {r5}
+                  </p>
+              </div>
+    </div>
+  </>
+
   )
 }
 
